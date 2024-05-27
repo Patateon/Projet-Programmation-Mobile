f4d6a67ddabcf983032f462216dbb4472f769b5b
besjb <jean-baptiste.bes@etu.umontpellier.fr>
2024-05-27 20:44:55 +0200

    add page des info user

--- app/src/main/java/com/main/projet_programmation_mobile/activity/UserInfoActivity.java
+++ app/src/main/java/com/main/projet_programmation_mobile/activity/UserInfoActivity.java
@@ -1,7 +1,11 @@
 package com.main.projet_programmation_mobile.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,5 +31,28 @@ public class UserInfoActivity extends AppCompatActivity {
 
         textViewUsername.setText("Username: " + username);
         textViewEmail.setText("Email: " + email);
+
+        // bouton home
+        ImageButton homeButton = (ImageButton) findViewById(R.id.home_button);
+        homeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainMenuActivity.class);
+            startActivity(intent);
+        });
+
+        Button logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent intent = new Intent(UserInfoActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
